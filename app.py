9a2faac8650fdcecbba49249de4fb03f850a2783
nanda9840-lang <nanda9840@gmail.com>
2026-02-17 00:25:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,46 +3,19 @@ import requests
 import os
 
 TOKEN = os.environ.get("TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.json
+    data = request.get_json(force=True)
+    
+    url = f"https://api.telegram.org/bot8296969716:AAH8HLVUTtwHbk2FBPsSxuNsUl7DE_fN10o/sendMessage"
+    r = requests.post(url, json=data)
 
-    symbol = data.get("symbol")
-    action = data.get("action")
-    entry  = data.get("entry")
-    tp1    = data.get("tp1")
-    tp2    = data.get("tp2")
-    tp3    = data.get("tp3")
-    sl     = data.get("sl")
-
-    message = f"""
-🎯 <b>Universal Adaptive Sniper PRO</b>
-
-<b>Symbol:</b> {symbol}
-<b>Action:</b> {action}
-
-🎯 Entry: {entry}
-✅ TP1: {tp1}
-✅ TP2: {tp2}
-✅ TP3: {tp3}
-🛑 SL: {sl}
-"""
-
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML"
-    }
-
-    requests.post(url, json=payload)
-
+    print("Telegram response:", r.text)
     return {"status": "ok"}
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=10000)
